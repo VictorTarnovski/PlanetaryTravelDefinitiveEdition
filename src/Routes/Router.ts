293de19d0db47959e7cc91a62acc94fc38e50bbc
VictorTarnovski <105019476+VictorTarnovski@users.noreply.github.com>
2022-10-05 22:49:25 -0300

    Ajustado a importação de Módulos

--- src/Routes/Router.ts
+++ src/Routes/Router.ts
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import { Router } from "express";
-=======
-import { Router} from "express";
->>>>>>> c37ea98c641304a972695daf91bcad068f591d4a
 
 import { CreateTravelController } from "../Controllers/CreateTravel";
 
