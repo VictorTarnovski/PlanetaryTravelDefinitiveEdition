642eb215e938b8e80da9a898287a9e81c3781a48
VictorTarnovski <victorcruztarnovski@gmail.com>
2022-10-19 08:28:34 -0300

    :test_tube: test: Fixing the validation on getName

--- Tests/getName.ts
+++ Tests/getName.ts
@@ -4,9 +4,9 @@ export default async function GetName(name: string) {
   let nameGet = await axios.get(`https://swapi.dev/api/people/?search=${name}`)
   let nameGetData = nameGet.data;
   let nameGetArray = nameGetData.results;
-  if (nameGetArray.lenght === 0) {
+  if (nameGetArray.lenght != 0) {
     let nameGetDataResults = nameGetData.results[0];
     let nameGetDataResultsName = nameGetDataResults.name
     return nameGetDataResultsName
-  } else {console.log("Não aceitamos macacos")};
+  } else {console.log("Pilot does not exist")};
 }
