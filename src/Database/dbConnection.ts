c720f4a0c1f48563db6debc646a74d654ef842fb
VictorTarnovski <victorcruztarnovski@gmail.com>
2022-11-17 12:57:57 -0300

    chore: change db host to Atlas

--- src/Database/dbConnection.ts
+++ src/Database/dbConnection.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function dbConnect() {
   mongoose
-    .connect("mongodb://127.0.0.1:27017/Planetary-Travels")
+    .connect("mongodb+srv://Tarnovski:E1u2t3u4@tarnovskiprojects.jrmrspf.mongodb.net/Applications")
     .then(() => {
       console.log("Database connection accepted");
     })
