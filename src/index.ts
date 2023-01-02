79f67835db5154ce30e579b610167fa991c6a1d9
VictorTarnovski <victorcruztarnovski@gmail.com>
2023-01-02 12:50:34 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { app } from "./app";
 
 const port = 66;
-const hostname = "https://ec2-18-229-141-143.sa-east-1.compute.amazonaws.com:";
+const hostname = "127.0.0.1:";
 
 app.listen(port, () => {
   console.log(`Server is running on ${hostname}${port}`);
