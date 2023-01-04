28ba756d8fa46042bf8b6a5d4f8b4258102f50ba
VictorTarnovski <victorcruztarnovski@gmail.com>
2023-01-04 09:02:43 -0300

    feat: made/people route to only return names

--- src/External Information/GetPeople.ts
+++ src/External Information/GetPeople.ts
@@ -1,8 +1,18 @@
 import axios from "axios";
 import { Request, Response } from "express";
 
+let url = 'https://swapi.dev/api/people/?page='
+
 export default async function GetPeople(req: Request, res: Response) {
-  let axiosGet: any = await axios.get("https://swapi.dev/api/people/");
-  let axiosGetData = axiosGet.data;
-  res.json(axiosGetData);
+  let pilots: any = []
+  for (let index = 1; index < 10; index++) {
+    let axiosGet: any = await axios.get(`${url}${index}`);
+    let axiosGetPeopleArray = axiosGet.data.results
+    for (let index = 0; index < axiosGetPeopleArray.length; index++) {
+      const people = axiosGetPeopleArray[index];
+      const peopleName = people.name
+      pilots.push(peopleName)
+    }
+  }
+  res.json(pilots);
 }
